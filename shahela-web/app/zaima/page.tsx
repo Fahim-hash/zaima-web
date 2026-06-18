82617ab6be0a6a9be3233c814c1f9e2080f1d0a2
Syed Fahim Muddasir <82995127+Fahim-hash@users.noreply.github.com>
2026-06-18 12:09:46 +0600

    Update page.tsx

--- shahela-web/app/zaima/page.tsx
+++ shahela-web/app/zaima/page.tsx
@@ -9,7 +9,7 @@ export default function ZaimaSpace() {
     "/images/494686438_959940016217240_6648276104973416309_n.jpg", 
     "/images/717337095_1162860456014927_7806065373298586581_n.jpg", 
     "/images/717337095_1162860456014927_7806065373298586581_n.jpg", 
-    "/images/zaima-5.jpg", 
+    "/images/_MG_0269.jpg", 
   ];
 
   // Interactive states
